d77697f12c83ddbd6df49c2e5f1c49cbc7ccd130
Juliana Rocumback <julianarocumback@proton.me>
2026-05-23 18:34:55 -0300

    Ajustando tamanho do icone do carrinho

--- src/components/header/carrinho/Carrinho.jsx
+++ src/components/header/carrinho/Carrinho.jsx
@@ -15,7 +15,7 @@ export default function Carrinho({carrinhoIsOpen, setCarrinhoIsOpen}) {
             <i className="fa-solid fa-cart-shopping"></i>
             {/* Mostrar quantidade de produtos no carrinho */}
             {items.length > 0 && 
-            <div  className={`bg-white border rounded-full flex justify-center items-center h-3 w-3 lg:h-4 lg:w-4 absolute top-0 -right-1 ${quantidadeProdutos > 9? 'text-[6px] lg:text-[9px]':'text-[8px] lg:text-[11px]'}`}>
+            <div  className={`bg-white border rounded-full flex justify-center items-center h-2 w-2 lg:h-4 lg:w-4 absolute top-0 -right-1 ${quantidadeProdutos > 9? 'text-[6px] lg:text-[9px]':'text-[8px] lg:text-[11px]'}`}>
                 {quantidadeProdutos}
             </div>
             }  
