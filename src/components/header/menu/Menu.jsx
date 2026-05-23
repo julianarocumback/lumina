e873a356a8332a5f5bc5ddc9be4ce00042c5b967
Juliana Rocumback <julianarocumback@proton.me>
2026-05-23 16:41:57 -0300

    Adicionando funcionalidade de menu responsivo

--- src/components/header/menu/Menu.jsx
+++ src/components/header/menu/Menu.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react'
+
 export default function Menu(){
+    const [isOpen, setIsOpen] = useState(false)
     
-    return(
-        <div className='md:hidden'>
-            <i className="fa-solid fa-bars"></i>
+    function handleIsOpen(){
+        setIsOpen(prev => !prev)
+    }
+
+    return (
+        <div>
+            <div onClick={handleIsOpen} className='md:hidden'>
+                <i className="fa-solid fa-bars"></i>
+            </div>
+
+            {isOpen&& <div className="h-screen w-50 bg-white z-50 absolute top-14 left-0 shadow-lg">
+                a
+            </div>}
+
+
         </div>
     )
 }
