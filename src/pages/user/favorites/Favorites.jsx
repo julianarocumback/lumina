9904ac3385944716ab7c451282dabf83323cd61c
Juliana Rocumback <julianarocumback@proton.me>
2026-05-27 22:16:20 -0300

    Ajustando título e descrição da lista de desejos

--- src/pages/user/favorites/Favorites.jsx
+++ src/pages/user/favorites/Favorites.jsx
@@ -8,15 +8,15 @@ export default function Favorites(){
         <div className="flex flex-col py-25 pl-20 pr-5 lg:pl-100 lg:pr-20  gap-8 top-14 w-full ">
 
                 <div className="flex flex-col gap-2">
-                    <h2 className="text-2xl font-semibold lg:text-4xl">Minha Lista de Desejos</h2>
-                    <p className="lg:text-lg">Guarde aqui os tesouros que você deseja iluminar sua biblioteca em breve. Cada página é um passo em direção à sabedoria.</p>
+                    <h2 className="text-2xl font-semibold lg:text-4xl"> Lista de Desejos</h2>
+                    <p className="lg:text-lg">Guarde aqui os tesouros que você deseja iluminar sua biblioteca em breve. </p>
                 </div>
 
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
                     {dadosCliente?.favoritos.map(favorito => {
                         return (
-                            <div className="flex lg:flex-col lg:h-120 gap-4 lg:gap-2 h-35 p-4 rounded-2xl bg-white shadow-xs border ">
+                            <div className="flex lg:flex-col lg:h-120 gap-4 lg:gap-2 h-40 p-4 rounded-2xl bg-white shadow-xs border ">
                                 <div className="w-25 lg:w-50 rounded-xl bg-gray-200 overflow-hidden">
                                     
                                     <img className='w-full h-full' src={favorito.img_url} alt="" />
