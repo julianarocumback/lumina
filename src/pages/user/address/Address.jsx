1d3b7ee3cb685b3b4b0fa24a484c78e830ece480
Juliana Rocumback <julianarocumback@proton.me>
2026-05-28 22:22:24 -0300

    Adicionando e organizando conteúdo no perfil e endereço

--- src/pages/user/address/Address.jsx
+++ src/pages/user/address/Address.jsx
@@ -1,6 +1,60 @@
 export default function Address(){
+
+    const enderecos = [
+        {
+            titulo: 'Casa',
+            rua: 'Rua das Graças',
+            numero: '777',
+            bairro: 'Jardim das Oliveiras',
+            cidade: 'São Paulo',
+            estado: 'SP',
+            cep: '01234-567',
+            status: 'PRINCIPAL',
+            icone: <i class="fa-solid fa-house-chimney"></i>,
+            tom: 'blue'
+        },
+        {
+            titulo: 'Trabalho',
+            rua: 'Rua das Bençãos',
+            numero: '111',
+            bairro: 'Jardim Grande',
+            cidade: 'São Paulo',
+            estado: 'SP',
+            cep: '01234-777',
+            status: null,
+            icone: <i class="fa-solid fa-briefcase"></i>,
+            tom: 'gray'
+        }
+    ]
+
     return(
-        <div className="relative h-[calc(100vh-56px)] top-14 w-70">
+        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-100 lg:pr-20 h-full">
+            <h1 className="text-2xl font-semibold">Endereços</h1>
+            {enderecos.map(endereco => {
+                return (
+                    <div className="bg-white p-6 rounded-2xl gap-4 flex flex-col">
+                        <div className="flex gap-4 justify-between items-center">
+                            <div className="flex items-center gap-4">
+                                <div className={`flex justify-center items-center text-${endereco.tom}-800 bg-${endereco.tom}-300 h-10 w-10 rounded-full`}>{endereco.icone}</div>
+                                <div>
+                                    <p className="text-lg font-semibold">{endereco.titulo}</p>
+                                    {endereco.status && <span className="text-xs font-semibold text-red-800 bg-red-300/30 px-2 py-0.5 rounded-full">{endereco.status}</span>}
+                                </div>
+                            </div>
+                            <div>
+                                aaa
+                            </div>
+                        </div>
+
+                        <div>
+                            <p>{endereco.rua}, {endereco.numero} - {endereco.bairro}, {endereco.cidade} - {endereco.estado}, {endereco.cep}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+                <div className="p-8 border">ccccccccccc</div>
+                <div className="p-8">ccccccccccc</div>
                 <div className="p-8">ccccccccccc</div>
             </div>
     )
--- src/pages/user/profile/Profile.jsx
+++ src/pages/user/profile/Profile.jsx
@@ -1,7 +1,128 @@
 export default function Perfil(){
     return(
-        <div className="relative h-[calc(100vh-56px)] top-14 w-70">
-                <div className="p-8">vvvvvvvvvvvv</div>
+        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-100 lg:pr-20 h-full">
+                <h1 className="text-2xl font-semibold">Configurações</h1>
+
+                <div className="flex flex-col gap-4">
+
+                    <h2 className="text-xl font-semibold">Dados pessoais</h2>
+
+                    <div className="bg-white border border-gray-100 rounded-2xl shadow-xs p-6 gap-8 flex flex-col">
+                        <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-semibold text-[11px] text-gray-500">NOME COMPLETO</h3>
+                            <p className="font-semibold">Beatriz Grace Cavalcante</p>
+                        </div>
+                        <div className="font-semibold text-blue-700">Editar</div>
+
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-semibold text-[11px] text-gray-500">CPF</h3>
+                            <p className="font-semibold">***.452.980-**</p>
+                        </div>
+                        <div className="font-semibold text-blue-700">Editar</div>
+
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-semibold text-[11px] text-gray-500">E-MAIL</h3>
+                            <p className="font-semibold">beatriz.grace@lumina.com</p>
+                        </div>
+                        <div className="font-semibold text-blue-700">Editar</div>
+
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-semibold text-[11px] text-gray-500">TELEFONE</h3>
+                            <p className="font-semibold">(11) 12345-6789</p>
+                        </div>
+                        <div className="font-semibold text-blue-700">Editar</div>
+
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div>
+                            <h3 className="font-semibold text-[11px] text-gray-500">DATA DE NASCIMENTO</h3>
+                            <p className="font-semibold">14 de Agosto, 1994</p>
+                        </div>
+                        <div className="font-semibold text-blue-700">Editar</div>
+                    </div>
+                    
+                    </div>
+                </div>
+
+                <div className="flex-col flex gap-4">
+
+                    <h2 className="text-xl font-semibold">Dados pessoais</h2>
+
+                    <div className="bg-white border border-gray-100 rounded-2xl shadow-xs p-6 gap-8 flex items-center">
+                        <div className="bg-blue-300/20 w-12 h-12 flex justify-center items-center rounded-2xl text-blue-800"><i class="fa-solid fa-lock"></i></div>
+                        <div>
+                            <h3 className="font-semibold">Alterar Senha</h3>
+                            <p className="text-xs text-gray-500">Última alteração há 3 meses</p>
+
+                        </div>
+                        <div>
+                            <button className="bg-blue-500 px-4 py-2 rounded-full font-semibold text-white">Alterar</button>
+                        </div>
+                        
+                    </div>
+
+                    <div className="bg-white border border-gray-100 rounded-2xl shadow-xs p-6 gap-8 flex items-center">
+                        <div className="bg-yellow-300/20 w-12 h-12 flex justify-center items-center rounded-2xl text-yellow-800"><i class="fa-solid fa-shield-halved"></i></div>
+                        <div>
+                            <h3 className="font-semibold">Alterar Senha</h3>
+                            <p className="text-xs text-gray-500">Proteção adicional ativada</p>
+
+                        </div>
+                        <div>
+                            <button className="bg-blue-500 px-4 py-2 rounded-full font-semibold text-white">Alterar</button>
+                        </div>
+                        
+                    </div>
+
+                </div>
+
+
+
+                <div className="flex flex-col gap-4">
+
+
+                <h2 className="text-xl font-semibold">Sessões ativas</h2>
+
+                <div className="bg-gray-100 border border-gray-100 rounded-2xl shadow-xs p-6 gap-8 flex items-center justify-between">
+                    <div className="flex gap-4 items-center">
+                    <div className="bg-white w-12 h-12 flex justify-center items-center rounded-2xl text-gray-800 text-xl"><i class="fa-solid fa-laptop"></i></div>
+                    <div>
+                        <h3 className="font-semibold">MackBook Pro</h3>
+                        <p className="text-xs text-gray-500">Safari . São Paulo . <span className="text-blue-700 italic font-semibold">Está sessão</span> </p>
+                    </div>
+
+                    </div>
+                    <div>
+                        <div className="font-semibold text-gray-500"><i class="fa-solid fa-arrow-right-from-bracket"></i></div>
+                    </div>
+                    
+                </div>
+
+                <div className="bg-gray-100 border border-gray-100 rounded-2xl shadow-xs p-6 gap-8 flex items-center justify-between">
+                    <div className="flex gap-4 items-center">
+                    <div className="bg-white w-12 h-12 flex justify-center items-center rounded-2xl text-gray-800 text-xl"><i class="fa-solid fa-mobile-screen-button"></i></div>
+                    <div>
+                        <h3 className="font-semibold">iPhone 15 Pro</h3>
+                        <p className="text-xs text-gray-500">Website . São Paulo . <span className="text-blue-700 italic font-semibold">Está sessão</span> </p>
+                    </div>
+
+                    </div>
+                    <div>
+                        <div className="font-semibold text-gray-500"><i class="fa-solid fa-arrow-right-from-bracket"></i></div>
+                    </div>
+                    
+                </div>
+
+                </div>
+
+                
             </div>
     )
 }
