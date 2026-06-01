307ee5042924a7f844510efc069bcaf16434da05
Juliana Rocumback <julianarocumback@proton.me>
2026-05-31 22:19:40 -0300

    Organizando o conteúdo da central de ajuda

--- src/pages/user/helpCenter/HelpCenter.jsx
+++ src/pages/user/helpCenter/HelpCenter.jsx
@@ -1,10 +1,10 @@
 export default function HelpCenter(){
     return(
-        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-100 lg:pr-20 h-full">
-                <div className="p-8 border border-gray-100 rounded-2xl gap-4 flex flex-col bg-[radial-gradient(at_0%_100%,#0288D120,transparent_30%),radial-gradient(at_100%_0%,#E91E6310,transparent_30%)] shadow-xs">
+        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-150 lg:pr-70 h-full">
+                <div className="p-8 lg:py-15 border border-gray-100 rounded-2xl gap-4 flex flex-col bg-[radial-gradient(at_0%_100%,#0288D120,transparent_30%),radial-gradient(at_100%_0%,#E91E6310,transparent_30%)] shadow-xs">
                     <h1 className="text-2xl font-semibold">Problemas com um pedido?</h1>
                     <p className="text-[14px]">Nossa equipe de suporte está pronta para resolver qualquer dúvida sobre suas entregas ou pagamentos.</p>
-                    <button className="flex gap-2 bg-pink-700 text-white font-semibold px-3 py-3 rounded-3xl justify-center">
+                    <button className="flex gap-2 bg-pink-700 text-white font-semibold px-3 py-3 rounded-3xl justify-center lg:w-75">
                         <div><i class="fa-solid fa-headset"></i></div>
                         Abrir chamado
                     </button>
@@ -12,9 +12,9 @@ export default function HelpCenter(){
 
                 <div>
 
-                <div className="py-8 flex flex-col gap-8 ">
+                <div className="py-8 flex flex-col lg:flex-row lg:justify-between gap-8 ">
                     <h2 className="text-xl font-semibold">Histórico de atendimento</h2>
-                    <div className="flex justify-between gap-4 bg-gray-200 p-1 rounded-2xl">
+                    <div className="flex justify-between gap-4 bg-gray-200 p-1 rounded-2xl lg:w-100">
                         <button className="w-full bg-white p-2 rounded-xl text-blue-700 font-semibold">Em aberto</button>
                         <button className="w-full rounded-xl font-semibold  text-gray-500">Concluído</button>
                     </div>
@@ -23,18 +23,22 @@ export default function HelpCenter(){
 
                 <div className="flex flex-col gap-4">
 
-<div className="p-8 shadow-xs border border-gray-100 flex flex-col bg-white rounded-2xl ">
-                    <div className="flex justify-between items-center">
-                        <p className="text-blue-700 text-xs font-medium">#CH-0002</p>
-                        <p className="bg-yellow-300 text-xs py-1 px-2 rounded-full font-semibold text-yellow-800">EM ANÁLISE</p>
-                    </div>
-                    <p className="text-xl font-semibold">Atraso na Entrega</p>
+                    <div className="p-8 shadow-xs border border-gray-100 flex justify-between  bg-white rounded-2xl ">
+                    <div className="flex gap-4 flex-col lg:flex-row lg:items-center lg:gap-8">
+                        <div className="flex flex-col lg:flex-row gap-2 lg:gap-8">
+                            <p className="text-blue-700 text-xs font-medium">#CH-0002</p>
+                            
+                            <div className="flex text-xs">
+                                <div><i class="fa-solid fa-calendar"></i></div>
+                                10 Out, 2023
+                            </div>
 
-                    <div className="flex mt-3 justify-between items-center">
-                        <div className="flex justify-between text-xs">
-                            <div><i class="fa-solid fa-calendar"></i></div>
-                            10 Out, 2023
                         </div>
+                      <p className="text-xl font-semibold">Atraso na Entrega</p>
+                    </div>
+
+                    <div className="flex flex-col mt-3 justify-between items-end gap-4 lg:flex-row">
+                       <p className="bg-yellow-300 text-xs py-1 px-2 rounded-full font-semibold text-yellow-800">EM ANÁLISE</p>
                         <button className="bg-gray-200 h-7 w-7 rounded-full">
                             <i class="fa-solid fa-angle-right"></i>
                         </button>
--- src/pages/user/payment/Payment.jsx
+++ src/pages/user/payment/Payment.jsx
@@ -3,7 +3,7 @@ export default function Payment(){
     const {dadosCliente} = useOutletContext()
     if(!dadosCliente) return
     return(
-        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-150 lg:pr-50 h-full lg:h-screen">
+        <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-150 lg:pr-70 h-full lg:h-screen">
             <div className="flex flex-col gap-4">
                 <h1 className="text-2xl lg:text-4xl font-semibold">Pagamentos</h1>
                 <h3 className="hidden md:block text-lg text-gray-600">Gerencie suas formas de pagamento e acompanhe suas compras com total transparência e segurança.</h3>
@@ -13,9 +13,9 @@ export default function Payment(){
                 <div className="flex flex-col gap-8">
                     <h2 className="text-xl lg:text-2xl font-semibold">Cartões Salvos</h2>
                     {/* CARTÕES  */}
-                    <div className="flex flex-col md:flex-row gap-4 flex-wrap">
+                    <div className="flex flex-col lg:flex-row gap-4 flex-wrap md:flex-row">
 
-                    <div className="flex flex-col md:flex-row gap-4 flex-wrap">
+                    <div className="flex flex-col lg:flex-row gap-4 flex-wrap md:flex-row">
                         {dadosCliente.cartoes.map(cartao => {
                             return (
                                 <div className="h-40 lg:h-45 lg:w-75 gap-2 w-full md:w-70 justify-center rounded-2xl bg bg-[radial-gradient(at_0%_0%,#000,transparent_100%),radial-gradient(at_100%_100%,#000,transparent_90%),radial-gradient(at_0%_0%,#000,transparent_80%)] shadow-lg p-4 flex flex-col">
@@ -55,14 +55,15 @@ export default function Payment(){
                     <h2 className="text-xl font-semibold">Transações recentes</h2>
                 
                     <div className="bg-white shadow-xs p-4 flex flex-col gap-6 rounded-2xl ">
-                
                         <div className=" flex justify-between">
-                            <div className="h-10 w-10 bg-gray-100 flex justify-center items-center rounded-xl">
-                                <i class="fa-solid fa-book-open"></i>
-                            </div>
-                            <div>
-                                <h3 className="text-[14px] font-semibold">O Peregrino</h3>
-                                <p className="text-[10px] ">24 Out 2025</p>
+                            <div className=" flex gap-4">
+                                <div className="h-10 w-10 bg-gray-100 flex justify-center items-center rounded-xl">
+                                    <i class="fa-solid fa-book-open"></i>
+                                </div>
+                                <div>
+                                    <h3 className="text-[14px] font-semibold">O Peregrino</h3>
+                                    <p className="text-[10px] ">24 Out 2025</p>
+                                </div>
                             </div>
                             <div>
                                 <p className="text-[14px] font-semibold">R$ 189,90</p>
@@ -73,18 +74,21 @@ export default function Payment(){
 
                        
                        <div className=" flex justify-between">
-                            <div className="h-10 w-10 bg-gray-100 flex justify-center items-center rounded-xl">
-                                <i class="fa-solid fa-book-open"></i>
-                            </div>
-                            <div>
-                                <h3 className="text-[14px] font-semibold">O Peregrino</h3>
-                                <p className="text-[10px] ">24 Out 2025</p>
+                            <div className=" flex gap-4">
+                                <div className="h-10 w-10 bg-gray-100 flex justify-center items-center rounded-xl">
+                                    <i class="fa-solid fa-book-open"></i>
+                                </div>
+                                <div>
+                                    <h3 className="text-[14px] font-semibold">O Peregrino</h3>
+                                    <p className="text-[10px] ">24 Out 2025</p>
+                                </div>
                             </div>
                             <div>
                                 <p className="text-[14px] font-semibold">R$ 189,90</p>
                                 <p className="text-[10px] text-right ">Concluído</p>
                             </div>
                         </div>
+
     
                     </div>
                     
