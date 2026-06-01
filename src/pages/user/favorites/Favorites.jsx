5d8c10744e7d4cde0bb281fb9c5e01e0a0a291a0
Juliana Rocumback <julianarocumback@proton.me>
2026-05-31 22:33:11 -0300

    Organizando o conteúdo da central de ajuda

--- src/pages/user/favorites/Favorites.jsx
+++ src/pages/user/favorites/Favorites.jsx
@@ -16,15 +16,15 @@ export default function Favorites(){
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8">
                     {dadosCliente?.favoritos.map(favorito => {
                         return (
-                            <div className="flex lg:flex-col lg:h-130 gap-4 lg:gap-2 h-40 p-4 rounded-2xl bg-white shadow-xs justify-between">
+                            <div className="flex flex-rol lg:flex-col lg:h-130 gap-4 lg:gap-2 h-40 p-4 rounded-2xl bg-white shadow-xs justify-between">
                                 <div className="w-30 lg:w-full lg:h-80 rounded-xl bg-gray-200 overflow-hidden">
                                     <img className='w-full h-full' src={favorito.img_url} alt="" />
                                 </div>
-                                <div>
-                                    <p className="text-lg/6 font-semibold truncate">{favorito.nome}</p>
-                                    <p className="text-xl lg:text-base font-bold text-blue-500">{favorito.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-                                </div>
                                 <div className="flex flex-col justify-between lg:gap-2">
+                                    <div>
+                                        <p className="text-lg/6 font-semibold truncate">{favorito.nome}</p>
+                                        <p className="text-xl lg:text-base font-bold text-blue-500">{favorito.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+                                    </div>
                                     <div className="flex gap-4 lg:flex-col">
                                         <button className="flex items-center lg:justify-center lg:py-3 gap-1 px-4 py-1.5 rounded-full bg-gradient-to-r from-blue-500 to-red-500 text-white "> 
                                             <div className="text-xs"><i class="fa-solid fa-plus"></i></div>
@@ -34,8 +34,6 @@ export default function Favorites(){
                                         <button className="hover:text-red-500 cursor-pointer hidden lg:block">Remover</button>
 
                                     </div>
-
-
                                 </div>
                             </div>
                                 
