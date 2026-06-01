baa532f49c5b38375308e48b1b96551cf02e973b
Juliana Rocumback <julianarocumback@proton.me>
2026-05-31 21:18:22 -0300

    Adicionando grid aos cards de cartões

--- src/pages/user/payment/Payment.jsx
+++ src/pages/user/payment/Payment.jsx
@@ -18,7 +18,7 @@ export default function Payment(){
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-8">
                         {dadosCliente.cartoes.map(cartao => {
                             return (
-                                <div className="h-45 gap-2  w-full lg:h-50 justify-center rounded-2xl bg bg-[radial-gradient(at_0%_0%,#000,transparent_100%),radial-gradient(at_100%_100%,#000,transparent_90%),radial-gradient(at_0%_0%,#000,transparent_80%)] shadow-lg p-4 flex flex-col">
+                                <div className="h-40 gap-2  w-full lg:h-50 justify-center rounded-2xl bg bg-[radial-gradient(at_0%_0%,#000,transparent_100%),radial-gradient(at_100%_100%,#000,transparent_90%),radial-gradient(at_0%_0%,#000,transparent_80%)] shadow-lg p-4 flex flex-col">
                             <div className="flex justify-between">
                             <div className="text-white"><i class="fa-brands fa-cc-visa"></i></div>
                             <span className="text-xs font-bold text-gray-400">{cartao.categoria}</span>
@@ -40,7 +40,7 @@ export default function Payment(){
                             )
                         })}
               
-                    <button className="h-45 flex-none lg:h-45 lg:w-75  w-full md:w-70 justify-center items-center gap-2 rounded-2xl p-4 flex flex-col border-dashed border border-gray-300">
+                    <button className="h-40 flex-none lg:h-45 lg:w-75  w-full md:w-70 justify-center items-center gap-2 rounded-2xl p-4 flex flex-col border-dashed border border-gray-300">
                             
                             <div className="flex justify-center items-center rounded-full bg-gray-200 w-10 h-10 text-gray-500"><i class="fa-solid fa-plus "></i></div>
                            
