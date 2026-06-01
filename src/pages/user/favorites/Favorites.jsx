e7d4e0e9c8f8cdd2a61f3829cee86a0dd6989d50
Juliana Rocumback <julianarocumback@proton.me>
2026-05-31 22:48:04 -0300

    Organizando o conteúdo dos favoritos

--- src/pages/user/favorites/Favorites.jsx
+++ src/pages/user/favorites/Favorites.jsx
@@ -16,11 +16,11 @@ export default function Favorites(){
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 overflow-hidden">
                     {dadosCliente?.favoritos.map(favorito => {
                         return ( 
-                            <div className="flex flex-rol lg:flex-col lg:h-130 gap-4 lg:gap-2 h-40 p-4 rounded-2xl bg-white shadow-xs lg:justify-between">
+                            <div className="flex flex-rol lg:flex-col lg:h-130 gap-4 lg:gap-2 h-40 p-4 rounded-2xl bg-white shadow-xs lg:justify-between ">
                                 <div className="w-20 flex-none  lg:w-full lg:h-80 rounded-xl bg-gray-200 overflow-hidden">
                                     <img className='w-full h-full' src={favorito.img_url} alt="" />
                                 </div>
-                                <div className="w-full flex flex-col justify-between lg:gap-2">
+                                <div className="w-full flex flex-col justify-between lg:gap-2 truncate">
                                     <div>
                                         <p className="text-lg/6 font-semibold truncate">{favorito.nome}</p>
                                         <p className="text-xl lg:text-base font-bold text-blue-500">{favorito.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
