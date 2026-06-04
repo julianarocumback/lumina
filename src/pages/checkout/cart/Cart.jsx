21ab6ac9b6fd3efcc99abe870351a6e040669a32
Juliana Rocumback <julianarocumback@proton.me>
2026-06-04 19:59:56 -0300

    Arrumando o espaçamento do conteúdo do checkout

--- src/pages/checkout/cart/Cart.jsx
+++ src/pages/checkout/cart/Cart.jsx
@@ -15,7 +15,7 @@ export default function Cart({lista, aumentarQuantidade, diminuirQuantidade, rem
                 </div>
             :etapa === 2 ?
                 <div className="flex flex-col lg:gap-8 lg:py-7 ">
-                    <div className="flex gap-8">
+                    <div className="flex flex-col lg:flex-row gap-8">
                         <Payment pagamento={pagamento} setPagamento={setPagamento} cupom={cupom} setCupom={setCupom} />
                         <OrderSummary lista={lista} frete={frete} cupom={cupom} listaOk={listaOk} enderecoOk={enderecoOk} pagamentoOk={pagamentoOk} verificar={verificar} etapa={etapa}/>
                     </div>
@@ -23,15 +23,15 @@ export default function Cart({lista, aumentarQuantidade, diminuirQuantidade, rem
                 </div>
             :etapa === 1 ?
                 <div className="flex flex-col lg:gap-8 lg:py-7">
-                    <div className="flex gap-8">
+                    <div className="flex flex-col lg:flex-row gap-8">
                         <Delivery endereco={endereco} setEndereco={setEndereco} frete={frete} setFrete={setFrete}/>
                         <OrderSummary lista={lista} frete={frete} cupom={cupom} listaOk={listaOk} enderecoOk={enderecoOk} pagamentoOk={pagamentoOk} verificar={verificar} etapa={etapa}/>
                     </div>
                     <BackLink/>
                 </div>
             :
-                <div className="flex flex-col lg:gap-8 lg:py-7 ">
-                    <div className="flex gap-8">
+                <div className="flex flex-col lg:gap-8 lg:py-7">
+                    <div className="flex flex-col lg:flex-row gap-8">
                         <ProductList lista={lista} aumentarQuantidade={aumentarQuantidade} diminuirQuantidade={diminuirQuantidade} removerDoCarrinho={removerDoCarrinho}/>
                         <OrderSummary lista={lista} frete={frete} cupom={cupom} listaOk={listaOk} enderecoOk={enderecoOk} pagamentoOk={pagamentoOk} verificar={verificar} etapa={etapa}/>
                     </div>
