fad9671fc6cda63006c6a53f52c6b3f20f1e20ca
Juliana Rocumback <julianarocumback@proton.me>
2026-06-13 15:40:29 -0300

    Arrumando espaçamento e tamanhos no checkout

--- src/pages/checkout/stepper/Stepper.jsx
+++ src/pages/checkout/stepper/Stepper.jsx
@@ -4,7 +4,7 @@ import ProgressLine from './progressLine/ProgressLine'
 
 export default function Stepper({etapa, setEtapa, listaOk, enderecoOk, pagamentoOk}){
     return(
-        <div className="flex fixed py-7 items-center justify-around lg:gap-8 lg:pt-30 lg:px-120 px-2 bg-gray-100 z-40 w-full left-0">
+        <div className="flex fixed py-7 items-center justify-around lg:gap-8 lg:pt-30 lg:px-120 px-2 bg-white border-b border-gray-200 lg:border-none lg:bg-gray-100 z-40 w-full left-0">
             <Step icone={<i class="fa-solid fa-bag-shopping"></i>} texto={'CARRINHO'} link={'/checkout'} estilo={`cursor-pointer ${listaOk ? 'bg-green-300' :etapa === 0 &&' border-blue-500 border-2'}`} etapa={etapa} setEtapa={setEtapa} valorEtapa={0} listaOk={listaOk} enderecoOk={enderecoOk} pagamentoOk={pagamentoOk}/>
             <ProgressLine estilo={`${listaOk && 'bg-green-300'}`}/>
             
