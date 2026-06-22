9c7c4c036b3063b8f5c3fba74bd4ff011d4bf636
Juliana Rocumback <julianarocumback@proton.me>
2026-06-22 20:59:27 -0300

    Adicionando favoritos recentes a página geral do usuário.

--- src/pages/user/geral/Geral.jsx
+++ src/pages/user/geral/Geral.jsx
@@ -6,7 +6,8 @@ import FavoritesPreview from './favoritesPreview/FavoritesPreview'
 
 export default function Geral(){
     const {dadosCliente} = useOutletContext()
-
+    if(!dadosCliente) return
+ 
     return (
         <div className="flex flex-col gap-8 lg:gap-8 pt-20 pb-5 lg:py-30 pl-20 pr-5 lg:pl-150 lg:pr-70 h-full">
             
@@ -14,7 +15,7 @@ export default function Geral(){
            
             <div className="flex-col lg:flex-row flex  gap-4 lg:gap-8">
                 <OrderPreview/>
-                <FavoritesPreview/>
+                <FavoritesPreview favorites={dadosCliente.favoritos}/>
             </div>
 
         </div>
--- src/pages/user/geral/favoritesPreview/FavoritesPreview.jsx
+++ src/pages/user/geral/favoritesPreview/FavoritesPreview.jsx
@@ -1,56 +1,31 @@
-export default function FavoritesPreview(){
-    const favoritos = [
-        {
-            imagem: '',
-            nome: 'Teste',
-            valor: 50
-        },
-        {
-            imagem: '',
-            nome: 'Teste',
-            valor: 50
-        },
-        {
-            imagem: '',
-            nome: 'Teste',
-            valor: 50
-        },
-        {
-            imagem: '',
-            nome: 'Teste',
-            valor: 50
-        },
-        {
-            imagem: '',
-            nome: 'Teste',
-            valor: 50
-        },
-
-    ]
-
+import { Link } from 'react-router-dom'
+export default function FavoritesPreview({favorites}){
+  
     return (
-        <div className="h-110 rounded-3xl lg:w-1/4 bg-white p-8 shadow-lg flex flex-col gap-4">
+        <div className="h-125 justify-center rounded-3xl lg:w-1/4 bg-white p-8 shadow-lg flex flex-col gap-4">
             <div className="flex justify-between">
                 <h3 className="font-semibold text-lg">Favoritos recentes</h3>
             </div>
 
-            <div className="flex flex-col h-full justify-between">    
-                {favoritos.map(favorito => {
-                    return(
-                    <div className="flex gap-4 items-center">
-                        <div className="border h-12 w-12 rounded-xl">
-                            {favorito.imagem}
-                        </div>
-                        <div className="">
-                            <p className="font-semibold">{favorito.nome}</p>
-                            <p className="text-blue-500 font-semibold">{favorito.valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
-                        </div>
-
-                    </div>
-                    )
-
-
-                })}             
+            <div className="flex flex-col h-full">    
+                
+                {[...favorites].reverse().map((favorite, index) => {
+                    if(index < 5) {
+                        return (
+                            <Link to={`/produto/${favorite.id}`}>
+                                <div className="  flex">
+                                    <div className="h-20 w-25  py-2 overflow-hidden ">
+                                        <img className="border border-gray-200 rounded-lg h-full" src={favorite.img_url} alt="imagem do produto favorito" />
+                                    </div>
+                                    <div className="flex flex-col justify-center gap-1 w-full">
+                                        <h4 className="text-[14px]/4">{favorite.nome}</h4>
+                                        <p className="font-semibold text-xs">{favorite?.valor.toLocaleString('PT-BR', {style: 'currency', currency: 'BRL'})}</p>
+                                    </div>
+                                </div>
+                            </Link>
+                        )
+                    }
+                })}         
             </div>
         </div>
     )
