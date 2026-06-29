bd55078839d82accf58d45676a8615fabee199f9
Juliana Rocumback <julianarocumback@proton.me>
2026-06-28 22:08:15 -0300

    Arrumando a formação do preço e mensagem no botão do produto

--- src/pages/product/productHero/ProductInfo/cart/Cart.jsx
+++ src/pages/product/productHero/ProductInfo/cart/Cart.jsx
@@ -2,8 +2,9 @@ import { useCart } from '../../../../../contexts/CartContext/CartContext'
 
 
 export default function Cart(produto){
-    const {addToCart} = useCart()
-    console.log(produto.produto)
+    const {addToCart,items} = useCart()
+    console.log(produto)
+    console.log(items)
 
     if(!produto){
         return
@@ -11,7 +12,7 @@ export default function Cart(produto){
 
     return (
         <div>
-            <button onClick={() => addToCart(produto.produto)} className="bg-gradient-to-r from-[#00639a] to-[#bc004b] w-full flex py-3 lg:py-4 rounded-4xl justify-center gap-3 text-xl font-semibold items-center text-white cursor-pointer"><span className="material-icons ">shopping_cart</span>Adicionar ao carrinho</button>
+            <button onClick={() => addToCart(produto.produto)} className="bg-gradient-to-r from-[#00639a] to-[#bc004b] w-full flex py-3 lg:py-4 rounded-4xl justify-center gap-3 text-xl font-semibold items-center text-white cursor-pointer"><span className="material-icons ">shopping_cart</span>{!items?.some(item => item.id === produto.id)? ' Carrinho': 'Adicionado'}</button>
         </div>
     )
 }
--- src/pages/product/productHero/ProductInfo/price/Price.jsx
+++ src/pages/product/productHero/ProductInfo/price/Price.jsx
@@ -1,7 +1,8 @@
+import {currencyFormatter} from '../../../../../utils/formatCurrency'
 export default function Price({produto}){
     return (
         <div>
-            <span className="text-2xl lg:text-3xl">{produto?.valor}</span>
+            <span className="text-2xl lg:text-3xl">{currencyFormatter(produto?.valor)}</span>
         </div>
     )
 }
