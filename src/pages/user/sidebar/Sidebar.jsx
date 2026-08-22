528c092e423430307ce7fd955b3458c662818572
Juliana Rocumback <julianarocumback@proton.me>
2026-08-22 19:02:04 -0300

    Mudando os nomes das variáveis para inglês e organizando as classes do tailwind no sidebar na página do usuário.

--- src/pages/user/sidebar/Sidebar.jsx
+++ src/pages/user/sidebar/Sidebar.jsx
@@ -1,66 +1,61 @@
 import { NavLink } from 'react-router-dom'
 
-export default function Sidebar({logout}) {
+export default function Sidebar() {
 
-    const opcoes = [
+    const navLinks = [
         {
             path: '/user',
             end: 'end',
-            icone: <span className="material-icons">dashboard</span>,
-            nome: 'Visão Geral'
+            icon: <span className='material-icons'>dashboard</span>,
+            name: 'Visão Geral'
         },
         {
             path: '/user/orders',
-            icone: <i className="fa-solid fa-box-open"></i>,
-            nome: 'Pedidos'
+            icon: <i className='fa-solid fa-box-open'></i>,
+            name: 'Pedidos'
         },
         {
             path: '/user/profile',
-            icone: <i className="fa-solid fa-user-shield"></i>,
-            nome: 'Perfil'
+            icon: <i className='fa-solid fa-user-shield'></i>,
+            name: 'Perfil'
         },
         {
             path: '/user/address',
     
-            icone: <i className="fa-solid fa-location-dot"></i>,
-            nome: 'Endereço'
+            icon: <i className='fa-solid fa-location-dot'></i>,
+            name: 'Endereço'
         },
         {
             path: '/user/payment',
-            icone: <i className="fa-solid fa-credit-card"></i>,
-            nome: 'Pagamento'
+            icon: <i className='fa-solid fa-credit-card'></i>,
+            name: 'Pagamento'
         },
         {
             path: '/user/favorites',
-            icone: <i className="fa-solid fa-heart"></i>,
-            nome: 'Favoritos'
+            icon: <i className='fa-solid fa-heart'></i>,
+            name: 'Favoritos'
         },
         {
             path: '/user/help-center',
-            icone: <i className="fa-solid fa-circle-question"></i>,
-            nome: 'Central de Ajuda'
+            icon: <i className='fa-solid fa-circle-question'></i>,
+            name: 'Central de Ajuda'
         }
     ]
 
     return (
-        <aside className="fixed h-[calc(100vh-56px)] lg:top-14 w-15 lg:w-80 bg-gray-100">
-            <div className="flex flex-col p-8 items-center lg:items-start justify-between w-full h-full">
-
-                {/* Navegação */}
-                <div className="flex flex-col gap-8">
-                    {opcoes.map(opcao=> {
+        <aside className='fixed w-15 h-[calc(100vh-56px)] bg-gray-100 lg:top-14 lg:w-80'>
+            <div className='flex flex-col justify-between items-center p-8 w-full h-full lg:items-start'>
+                {/* Navigation */}
+                <div className='flex flex-col gap-8'>
+                    {navLinks.map(link => {
                         return (
-                            <NavLink key={opcao.path} to={opcao.path} end={opcao.end} className={({isActive}) => `flex gap-4 text-[#474747] items-center ${isActive && 'text-red-500'}`}>
-                                <span className="">{opcao.icone}</span>
-                                <span className="hidden lg:inline"> {opcao.nome}</span>
-                                
+                            <NavLink key={link.path} to={link.path} end={link.end} className={({isActive}) => `flex gap-4  items-center ${isActive ? 'text-red-500': 'text-[#474747]'}`}>
+                                <span className=''>{link.icon}</span>
+                                <span className='hidden lg:inline'> {link.name}</span>
                             </NavLink>
                         )
                     })}
                 </div>
-
-               
-                    
             </div>
         </aside>
     )
