d38c0c1bbfd56a4f9e6cb42e0e79143dd5347a7c
Juliana Rocumback <julianarocumback@proton.me>
2026-09-02 21:51:33 -0300

    Arrumando  espaçamento da página favoritos do usuário para todos os tamanhos de tela.

--- src/pages/user/favorites/Favorites.jsx
+++ src/pages/user/favorites/Favorites.jsx
@@ -20,17 +20,17 @@ export default function Favorites(){
     }
 
     return (
-        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.7}} className={`flex flex-col gap-8 ml-15 p-4 pb-18 sm:p-16 sm:pb-30 md:px-32 lg:ml-80 lg:p-16 lg:pt-30 xl:p-32 xl:pb-18 2xl:px-70 `}>
+        <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.7}} className={`flex flex-col gap-8 ml-15 p-4 pb-18 sm:p-8 sm:pb-30 md:px-16 lg:ml-80 lg:pt-30 xl:p-32 xl:pb-18 2xl:px-70 `}>
             <div className='flex flex-col gap-2'>
                 <h2 className='text-2xl font-semibold lg:text-2xl'> Lista de Desejos</h2>
                 <p className='lg:text-lg'>Guarde aqui os tesouros que você deseja iluminar sua biblioteca em breve. </p>
             </div>
-            <div className='grid grid-cols-1 overflow-hidden gap-4 py-2 md:grid-cols-2 lg:grid-cols-[repeat(4,minmax(200px,1fr))]'>
+            <div className='grid grid-cols-1 overflow-hidden gap-4 md:gap-8 py-2 md:grid-cols-2 lg:grid-cols-[repeat(4,minmax(200px,1fr))]'>
                 <AnimatePresence>
                 {favorites.length > 0 ? 
                 favorites.map(favorite => {
                     return ( 
-                        <motion.div initial={{opacity:0, y:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:0.5}} layout key={favorite.id} className='relative flex flex-rol gap-4 h-40 p-4 bg-white rounded-2xl shadow lg:flex-col lg:justify-between lg:gap-4 lg:h-fit '>
+                        <motion.div initial={{opacity:0, y:0}} animate={{opacity:1}} exit={{opacity:0}} transition={{duration:0.5}} layout key={favorite.id} className='relative flex flex-rol gap-4 h-40 p-4 bg-white rounded-2xl shadow lg:flex-col lg:justify-between lg:gap-8 lg:h-fit '>
                             <button className='absolute hidden top-7 right-7 w-8 h-8 text-black/50 bg-white/90 rounded-2xl transition-all shadow cursor-pointer lg:block hover:text-red-600 hover:bg-red-200 hover:scale-110' onClick={() => handleRemoveFromFavorites(favorite)}><i className="fa-solid fa-heart-crack"></i></button>
                             <div className='flex-none overflow-hidden w-20 bg-gray-200 rounded-xl lg:w-fit lg:h-75'>
                                 <img className='w-full h-full' src={favorite.img_url} alt='Capa do livro' />
