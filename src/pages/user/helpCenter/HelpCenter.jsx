c5ad6f062ba09cf9bf88cc3695c722974317633f
Juliana Rocumback <julianarocumback@proton.me>
2026-09-02 23:05:26 -0300

    Arrumando  espaçamento e tamanho da fonte do texto no help center do usuário.

--- src/pages/user/helpCenter/HelpCenter.jsx
+++ src/pages/user/helpCenter/HelpCenter.jsx
@@ -17,9 +17,9 @@ export default function HelpCenter(){
             {/* SUPPORT HISTORY SECTION */}
             <div>
                 <div className='flex flex-col items-center gap-8 py-8 sm:flex-row lg:justify-between'>
-                    <h2 className='text-xl font-semibold'>Histórico de atendimento</h2>
+                    <h2 className='sm:text-lg lg:text-xl font-semibold'>Histórico de atendimento</h2>
                     {/* Filter tabs */}
-                    <div className='flex justify-between gap-4 w-full p-1 bg-gray-200 rounded-2xl lg:w-100 '>
+                    <div className='flex justify-between gap-4 w-full p-1 bg-gray-200 rounded-2xl sm:w-70 lg:w-100 '>
                         <button className='w-full p-2 text-blue-700 font-semibold bg-white rounded-xl'>Em aberto</button>
                         <button className='w-full font-semibold text-gray-500 rounded-xl'>Concluído</button>
                     </div>
